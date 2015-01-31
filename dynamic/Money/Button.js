cf678643673b5673dc4b464fc3b81da317cb0e35
James Bjorkman <james@bjorkman.ca>
2015-02-01 09:54:45 +1100

    MOTHER FUCKING PAYMENTS WORK :+1: :+1: :+1:

--- dynamic/Money/Button.js
+++ dynamic/Money/Button.js
@@ -1,13 +1,20 @@
 "use strict";
 
 var React = require("react")
+var f     = require("../factory")
+var I     = f("input", {type: "hidden"})
 
 module.exports = React.createClass({
   displayName : "Money/Button",
 
   render() {
-    var {amount} = this.props
-    return <form>
+    var {amount, email} = this.props
+    return <form method="post" action="https://www.paypal.com/cgi-bin/webscr" target="_blank">
+      <I name="cmd"           value="_donations"/>
+      <I name="business"      value={email}/>
+      <I name="item_name"     value="Federation"/>
+      <I name="currency_code" value="USD"/>
+      {amount !== "other" && <I name="amount" value={amount}/>}
       <button className="MoneyButton">
         {amount === "other" ? "Other" : "$"+amount}
       </button>
