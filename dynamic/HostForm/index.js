856581ab8a982966f10c26e22139d99df3a087d2
James Bjorkman <james@bjorkman.ca>
2015-01-31 22:40:50 +1100

    can add hosts

--- dynamic/HostForm/index.js
+++ dynamic/HostForm/index.js
@@ -1,9 +1,12 @@
 "use strict";
 
 require("./styles.css")
-  
-var React   = require("react")
-var factory = require("../factory")
+
+var React     = require("react")
+var factory   = require("../factory")
+var actions   = require("./actions")
+var store     = require("./store")
+var subscribe = require("oro-dispatcher/lib/subscribe")
 
 var Form       = factory("div",    "HostForm")
 var Input      = factory("input",  "HostFormInput")
@@ -11,16 +14,33 @@ var Submit     = factory("button", "HostFormSubmit")
 var HostIcon   = factory("i",      "fa fa-cube")
 var SubmitIcon = factory("i",      "fa fa-plus")
 
+function state() {
+  return {
+    host    : store.getHost(),
+    isValid : store.isValid()
+  }
+}
+
 module.exports = React.createClass({
   display: "HostForm",
+  mixins : [subscribe(store, state)],
 
   render() {
-    return <Form>
+    var {state, updateHost, submit} = this
+    var {host, isValid}             = state
+
+    return <Form onSubmit={submit}>
       <HostIcon/>
-      <Input placeholder="Add Host"/>
-      <Submit>
-        <SubmitIcon/>
-      </Submit>
+      <Input placeholder="Add Host" value={host} onChange={updateHost}/>
+      {isValid && <Submit onClick={submit}><SubmitIcon/></Submit>}
     </Form>
+  },
+
+  updateHost(e) { actions.updateHost(e.target.value) },
+
+  submit(e) {
+    e.preventDefault()
+    var {isValid, host} = this.state
+    if (isValid) actions.submit(host);
   }
 });
--- dynamic/HostForm/store.js
+++ dynamic/HostForm/store.js
@@ -1,3 +1,36 @@
 "use strict";
 
 var projection = require("oro-dispatcher/lib/projection")
+
+var __Host, __Length, __Valid
+setHost("")
+
+function setHost(host) {
+  __Host   = host
+  __Length = host.length
+  __Valid  = __Length > 0
+}
+
+module.exports = projection("HOST_FORM", {
+
+  getHost()   { return __Host },
+  getLength() { return __Length },
+  isValid()   { return __Valid }
+
+}, function(payload) {
+  var {actionType, host} = payload.action
+
+  console.log(actionType, payload.action)
+
+  switch (actionType) {
+    case "HOST_FORM_UPDATE_HOST":
+      setHost(host)
+      this.broadcast()
+      break;
+
+    case "HOST_FORM_SUBMIT":
+      setHost("")
+      this.broadcast()
+      break;
+  }
+})
