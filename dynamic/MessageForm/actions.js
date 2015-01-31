8dff6ee0185f7076603ac0fcc88b84a52c6984e3
James Bjorkman <james@bjorkman.ca>
2015-01-31 17:37:03 +1100

    Frontend Api Stuff

--- dynamic/MessageForm/actions.js
+++ dynamic/MessageForm/actions.js
@@ -1,8 +1,20 @@
 "use strict";
 
 var send = require("oro-dispatcher/lib/send")
+var post = require("oro-xhr/lib/post")
+
+function postMessage(text) {
+  return post("/api/v1/messages", {text})
+};
+
+function postMessageSuccess() {
+  send("MESSAGE_FORM_SUBMIT")
+}
 
 module.exports = {
   updateMessage(message) { send("MESSAGE_FORM_UPDATE_MESSAGE", {message}) },
-  submit(message)        { send("MESSAGE_FORM_SUBMIT", {message}) },
+
+  submit(text) {
+    postMessage({text}).then(postMessageSuccess);
+  },
 }
