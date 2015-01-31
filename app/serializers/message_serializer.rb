42eab8efca2b791a7ab991530a9d3ad54c8ac251
Joel Richards <joelr@everydayhero.com.au>
2015-01-31 19:35:58 +1100

    Include sender name

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,8 +1,17 @@
 class MessageSerializer < ActiveModel::Serializer
   require 'digest'
-  attributes :text, :sender_host, :filter, :created_at, :uuid
+  attributes :text, :sender_host, :filter, :created_at, :uuid, :sender_name
 
   def created_at
     object.created_at.to_i * 1000
+  end
+
+  def sender_name
+    host = Host.find_host(object.sender_host)
+    host.name
+  end
+
+  def include_sender_name?
+    !sender_name.blank?
   end
 end
