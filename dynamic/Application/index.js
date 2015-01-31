66aade9fecdbc272a220361856a46b565728cd88
James Bjorkman <james@bjorkman.ca>
2015-01-31 21:27:11 +1100

    sidebar

--- dynamic/Application/index.js
+++ dynamic/Application/index.js
@@ -5,12 +5,12 @@ require("./styles.css");
 var React   = require("react")
 var factory = require("../factory")
 
-var Main = require("../Main")
+var Main    = require("../Main")
+var Sidebar = require("../Sidebar")
 
 var App     = factory("div", "Application")
 var Header  = factory("h1",  "Application_header")
 var Content = factory("div", "Application_content")
-// var Side    = factory("div", "Application_side")
 
 module.exports = React.createClass({
   displayName : "Application",
@@ -21,6 +21,7 @@ module.exports = React.createClass({
         <Header>Federation</Header>
         <Main/>
       </Content>
+      <Sidebar/>
     </App>
   }
 });
