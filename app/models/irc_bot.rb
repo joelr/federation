1608ecd3aefcc7cb6c1d595f25e63721715f5e71
Joel Richards <joelr@everydayhero.com.au>
2015-02-01 06:23:21 +1100

    RADIO TRANSMISSION AW YEA

--- app/models/irc_bot.rb
+++ app/models/irc_bot.rb
@@ -1,16 +1,34 @@
 class IrcBot
   require 'cinch'
 
-  def initialize server, channel, nick
+  def self.boot
     bot = Cinch::Bot.new do
+      $message_ids = []
       configure do |c|
-        c.server = "irc.z.je"
+        c.nick = "federation-relay"
+        c.server = "localhost"
+        c.port = 2000
         c.channels = ["#battlehack"]
       end
 
       on :message, "hello" do |m|
-        m.reply "Hello, #{m.user.nick}"
+        
+        Timer(5) { 
+          messages = Message.fetch_from_url "http://joel.coopcoopcoop.net", "user", "lol"
+          messages = messages.reject{|mess| $message_ids.include?(mess.uuid)}
+          messages.each do |message|
+            $message_ids << message.uuid
+            Channel("#battlehack").send("<#{message.sender_host}>: #{message.text}") 
+          end
+          
+        }
       end
+
+      on :message, "quit" do |m|
+        quit
+      end
+
+
     end
 
     bot.start
