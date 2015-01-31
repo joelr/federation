faf883f51268a9217f5afbcbcb5aad84bfaec759
Joel Richards <joelr@everydayhero.com.au>
2015-02-01 08:55:03 +1100

    Post broadcast

--- dynamic/HostForm/actions.js
+++ dynamic/HostForm/actions.js
@@ -2,6 +2,7 @@
 
 var send = require("oro-dispatcher/lib/send")
 var post = require("oro-xhr/lib/post")
+var get = require("oro-xhr/lib/get")
 
 function postHost(host) {
   return post("/api/v1/hosts/add", {host})
@@ -11,7 +12,11 @@ function postHostSuccess() {
   send("HOST_FORM_SUBMIT")
 }
 
+function broadcast() {
+  return get("/api/v1/broadcast");
+}
+
 module.exports = {
   updateHost(host) { send("HOST_FORM_UPDATE_HOST", {host}) },
-  submit(host)     { postHost(host).then(postHostSuccess) }
+  submit(host)     { postHost(host).then(postHostSuccess).then(broadcast) }
 }
