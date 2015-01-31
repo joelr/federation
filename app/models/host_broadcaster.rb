bd8d8e4c2d9c145a1ba900dd7d04f3c3dacaefe3
Joel Richards <joelr@everydayhero.com.au>
2015-02-01 08:47:02 +1100

    more recursion

--- app/models/host_broadcaster.rb
+++ app/models/host_broadcaster.rb
@@ -1,17 +1,18 @@
 class HostBroadcaster
 
-  def self.broadcast host, method = 'host_register'
+  def self.broadcast host, method = 'host_register', custom_host = nil
     FederationBroadcaster.hosts_for_broadcast.each do |host|
       puts "broadcasting?"
       puts host.inspect
+      host_to_publish = custom_host || host
       r = FederationBroadcaster.publish_to_host({
         type: method,
         data: {
-          host: host.host,
-          name: host.name,
-          url: host.url,
-          charity_id: host.charity_id,
-          has_email: host.has_email
+          host: host_to_publish.host,
+          name: host_to_publish.name,
+          url: host_to_publish.url,
+          charity_id: host_to_publish.charity_id,
+          has_email: host_to_publish.has_email
         }
       }, host)
       if r.presence && r["hosts"]
@@ -34,7 +35,9 @@ class HostBroadcaster
   #broadcast all sends a discover, which doesn't republish.
   def self.broadcast_all
     Host.all.each do |host|
-      broadcast host, 'discover'
+      Host.local_hosts.each do |host|
+        broadcast host, 'discover', host
+      end
     end
   end
 end
