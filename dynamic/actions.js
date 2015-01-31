431c5a8850e3e31892b3eecd8738a2b29b257a01
James Bjorkman <james@bjorkman.ca>
2015-02-01 06:51:37 +1100

    poll hosts again

--- dynamic/actions.js
+++ dynamic/actions.js
@@ -3,12 +3,25 @@
 var send = require("oro-dispatcher/lib/send")
 var get  = require("oro-xhr/lib/get")
 
+// quick
 function getMessages()            { return get("/api/v1/messages") }
 function getMessagesSuccess(data) { send("MESSAGES_UPDATE", JSON.parse(data.text)) }
 
-function allThePolls() {
+function quickPolls() {
   getMessages().then(getMessagesSuccess)
 }
 
-allThePolls()
-setInterval(allThePolls, 1750)
+quickPolls()
+setInterval(quickPolls, 1750)
+
+
+// Not so quick
+function getHosts()               { return get("/api/v1/hosts") }
+function getHostsSuccess(data)    { send("HOSTS_UPDATE", JSON.parse(data.text)) }
+
+function notSoQuickPolls() {
+  getHosts().then(getHostsSuccess)
+}
+
+notSoQuickPolls()
+setInterval(notSoQuickPolls(), 5000)
